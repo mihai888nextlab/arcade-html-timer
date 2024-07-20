bad95c0b57bc912ea1d917cde9ca25e21a3c8324
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2024-07-20 12:32:02 +0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # arcade-html-timer
+
+This is a simple timer made with html, css and js. You can play with it if you want, have fun!
--- script.js
+++ script.js
@@ -24,6 +24,8 @@ function updateTimer() {
   if (remainingTime <= 0) {
     clearInterval(timer);
     alert("Time is up!");
+    document.getElementById("timer").classList.add("hidden");
+    document.getElementById("form").classList.remove("hidden");
     return;
   }
 
@@ -51,7 +53,4 @@ function stopTimer() {
   document.getElementById("display-hours").textContent = "00";
   document.getElementById("display-minutes").textContent = "00";
   document.getElementById("display-seconds").textContent = "00";
-
-  document.getElementById("timer").classList.add("hidden");
-  document.getElementById("form").classList.remove("hidden");
 }
